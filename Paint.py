6873a7f0a398865627fac3ac75ec3e92a911b6e6
A01798671 <a01798671@tec.mx>
2023-05-10 10:47:39 -0600

    Comentarios de Paint

--- Paint.py
+++ Paint.py
@@ -9,71 +9,74 @@ Exercises
 5. Add width parameter.
 """
 
-from turtle import *
+from turtle import * #Importamos turtle
 
-from freegames import vector
+from freegames import vector #importamos vector de freegames
 
-# Se define la funcion que dibuja la linea
+"""Funcion que dibuja la linea"""
 def line(start, end):
     """Draw line from start to end."""
-    up()
-    goto(start.x, start.y)
-    down()
-    goto(end.x, end.y)
+    up() #Levantamos la tortuga
+    goto(start.x, start.y) #Es el punto del que comienza la linea
+    down() #Bajamos la tortuga
+    goto(end.x, end.y) #Es el punto en el que termina la linea 
 
-
+"""Funcion que dibuja el cuadrado"""
 def square(start, end):
     """Draw square from start to end."""
-    up()
-    goto(start.x, start.y)
-    down()
-    begin_fill()
+    up() #Levantamos la tortuga
+    goto(start.x, start.y) #Es el punto en el que comienza
+    down() #Bajamos la tortuga
+    begin_fill() #comenzamos a rellenar la figura
 
+    """Ciclo for que dibuja el cuadrado"""
     for count in range(4):
-        forward(end.x - start.x)
-        left(90)
+        forward(end.x - start.x) #Para avanzar la distancia de los lados del cuadrado
+        left(90) #Gira 90 grados a la izquierda
 
-    end_fill()
+    end_fill() #terminamos de rellenar la figura
 
-
+"""Funcion que dibuja el circulo"""
 def cir(start, end):
     """Draw circle from start to end."""
     #t = turtle.Turtle()
-    up()
-    goto(start.x, start.y)
-    down()
-    begin_fill()
-    r = (end.x-start.x)
-    circle(r)
-    end_fill()
+    up() #Levantamos la tortuga
+    goto(start.x, start.y) #Es el punto en el que comienza
+    down() #Bajamos la tortuga
+    begin_fill() #comenzamos a rellenar la figura
+    r = (end.x-start.x) #calculamos el radio que va a tener el circulo
+    circle(r) #Dibuja el circulo utilizando el radio ya calculado
+    end_fill() #terminamos de rellenar la figura
 
-
+"""Funcion que dibuja el rectangulo"""
 def rectangle(start, end):
     """Draw rectangle from start to end."""
-    up()
-    goto(start.x, start.y)
-    down()
-    begin_fill()
+    up() #Levantamos la tortuga
+    goto(start.x, start.y) #Es el punto en el que comienza
+    down() #Bajamos la tortuga
+    begin_fill() #comenzamos a rellenar la figura
+    """Ciclo for que dibuja el rectangulo"""
     for count in range(2):
-        forward(end.x - start.x)
-        left(90)
-        forward(end.y - start.y)
-        left(90)
-    end_fill()
+        forward(end.x - start.x) #Para avanzar la distancia de las lineas horizontales
+        left(90) #Gira 90 grados a la izquierda
+        forward(end.y - start.y) #Para avanzar la distancia de las lineas verticales
+        left(90) #Gira 90 grados a la izquierda
+    end_fill() #terminamos de rellenar la figura
 
-
+"""Funcion que dibuja el triangulo"""
 def triangle(start, end):
     """Draw triangle from start to end."""
-    up()
-    goto(start.x, start.y)
-    down()
-    begin_fill()
+    up() #Levantamos la tortuga
+    goto(start.x, start.y) #Es el punto en el que comienza
+    down() #Bajamos la tortuga
+    begin_fill() #comenzamos a rellenar la figura
+    """Ciclo for que dibuja el triangulo"""
     for i in range(3):
-        forward(end.x - start.x)
-        left(120)
-    end_fill()
+        forward(end.x - start.x) #Para avanzar la distancia de los lados del cuadrado
+        left(120) #Gira 120 grados a la izquierda
+    end_fill() #terminamos de rellenar la figura
 
-
+"""Funcion que agarra las coordenadas x e y"""
 def tap(x, y):
     """Store starting point or draw shape."""
     start = state['start']
@@ -86,7 +89,7 @@ def tap(x, y):
         shape(start, end)
         state['start'] = None
 
-
+"""Funcion almacena todo"""
 def store(key, value):
     """Store value in state at key."""
     state[key] = value
@@ -97,15 +100,17 @@ setup(420, 420, 370, 0)
 onscreenclick(tap)
 listen()
 onkey(undo, 'u')
-onkey(lambda: color('black'), 'K')
-onkey(lambda: color('white'), 'W')
-onkey(lambda: color('green'), 'G')
-onkey(lambda: color('blue'), 'B')
-onkey(lambda: color('red'), 'R')
-onkey(lambda: color('magenta'), 'M')
-onkey(lambda: store('shape', line), 'l')
-onkey(lambda: store('shape', square), 's')
-onkey(lambda: store('shape', cir), 'c')
-onkey(lambda: store('shape', rectangle), 'r')
-onkey(lambda: store('shape', triangle), 't')
+"""Colores con sus respectivas teclas"""
+onkey(lambda: color('black'), 'K') #Color negro con la tecla K
+onkey(lambda: color('white'), 'W') #Color blanco con la tecla W
+onkey(lambda: color('green'), 'G') #Color verde con la tecla G
+onkey(lambda: color('blue'), 'B') #Color azul con la tecla B
+onkey(lambda: color('red'), 'R') #Color rojo con la tecla R
+onkey(lambda: color('magenta'), 'M') #Color magenta con la tecla M
+"""Figuras con sus respectivas teclas"""
+onkey(lambda: store('shape', line), 'l') #Linea con tecla l
+onkey(lambda: store('shape', square), 's') #Cuadrado con tecla s
+onkey(lambda: store('shape', cir), 'c') #Circulo con tecla c
+onkey(lambda: store('shape', rectangle), 'r') #Rectangulo con tecla r
+onkey(lambda: store('shape', triangle), 't') #Triangulo con tecla t
 done()
